42fb94a48f93ca369e949ee72432d1f842fca4dc
Mona Lisa <alitaha.1187@gmail.com>
2019-02-04 01:01:37 -0500

    the sqrt function has been tested and the exceptions have been handled

--- recursion.java
+++ recursion.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class recursion{
     /*You may write additional private methods */
 
@@ -6,15 +7,19 @@ public class recursion{
      *precondition: n is non-negative
 
     */
+    public static void main(String[] args) {
+      System.out.println("sqrt of "+ 2 +" is " +sqrt(2,.00001));
+    }
     public static double sqrt(double n, double tolerance){
       //sqrtH is a helper recursive function with more parameters
+      if (n==0) return 0;
       return sqrtH(n,1.,tolerance);
     }
   //absolute value method
     private static double abs(double n){if(n<0) return n*-1;return n;}
     private static double sqrtH(double n, double guess, double tolerance){
-      if(abs((guess*guess-n)/n)<tolerance)return guess;
-      sqrtH(n,( n / guess + guess) / 2),tolerance);
+      if(abs((guess*guess-n)/n)<tolerance/100)return guess;
+      return sqrtH(n, (n/guess + guess)/ 2. ,tolerance);
     }
 
     /*Recursively find the n'th fibbonaci number in linear time
@@ -23,12 +28,12 @@ public class recursion{
      *fib(5) = 5
      *precondition: n is non-negative
      */
-    public static int fib(int n){
+  /*  public static int fib(int n){
 
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(int n){
-    }
+  /*  public static ArrayList<Integer> makeAllSums(int n){
+  }*/
 
 }
