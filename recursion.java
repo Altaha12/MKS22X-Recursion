273c6c7fc91084c00799731cc3e82cfc4fbbca54
Mona Lisa <alitaha.1187@gmail.com>
2019-02-04 01:22:13 -0500

    built fib function with linear time, overflow causes negative numbers at 48th number in the sequence.4 more lines of code!

--- recursion.java
+++ recursion.java
@@ -9,6 +9,12 @@ public class recursion{
     */
     public static void main(String[] args) {
       System.out.println("sqrt of "+ 2 +" is " +sqrt(2,.00001));
+      System.out.println("fib of 5 is "+ fib(0));
+      System.out.println("fib of 5 is "+ fib(1));
+      System.out.println("fib of 5 is "+ fib(5));
+      System.out.println("fib of 5 is "+ fib(49));
+      System.out.println("fib of 5 is "+ fib(13));
+      System.out.println("fib of 5 is "+ fib(19));
     }
     public static double sqrt(double n, double tolerance){
       //sqrtH is a helper recursive function with more parameters
@@ -28,9 +34,17 @@ public class recursion{
      *fib(5) = 5
      *precondition: n is non-negative
      */
-  /*  public static int fib(int n){
-
+   public static int fib(int n){
+     //trueFib is the helper function
+     if(n==0)return 0;
+     if(n==1)return 1;
+     return trueFib(0,1,2,n);
     }
+  private static int trueFib(int first, int second, int index, int target){
+    //Comment: keeps adding remembring terms from before, goes from the bottom up.
+    if(index==target)return first+second;
+    return trueFib(second,first+second,index+1,target);
+  }
 
     /*As Per classwork*/
   /*  public static ArrayList<Integer> makeAllSums(int n){
